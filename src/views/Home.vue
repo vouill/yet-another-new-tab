450a9c698c8a14a334e1c839b3ffb27267fecf56
vouill <dev@vouill.com>
2018-06-21 00:56:24 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title">Websites:</div>
+    <div class="website" v-for="website in websites"><img height="14px" :src="`https://logo.clearbit.com/${website.url.replace(/(^\w+:|^)\/\//, '').split('/')[0]}`"><a :href="website.url">{{website.label}}</a></div>
+    <form @submit.prevent="addWebsite" class="website">
+      <input type="url" v-model="urlInput" required placeholder="url">
+      <input v-model="labelInput"  type="text" required placeholder="label">
+      <button type="submit">submit</button>
+    </form>
   </div>
 </template>
 
@@ -11,8 +16,37 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
+  data() {
+      const websites = localStorage.getItem('websites')
+    return {
+      websites: websites && JSON.parse(websites) || [],
+      urlInput: "",
+      labelInput: ""
+    };
+  },
   components: {
     HelloWorld
-  }
+  },
+    methods: {
+        addWebsite() {
+            this.websites.push({ url: this.urlInput, label: this.labelInput })
+            localStorage.setItem('websites',JSON.stringify(this.websites))
+            this.urlInput = ''
+            this.labelInput = ''
+        }
+    },
 };
 </script>
+
+<style scoped lang="scss">
+.website {
+  display: flex;
+  align-items: center;
+  a{
+    margin-left: 0.5em;
+  }
+}
+  input {
+    margin-right: 1em;
+  }
+</style>
