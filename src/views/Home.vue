349268f2fd064443f91682791e3c4cbdc526d050
vouill <dev@vouill.com>
2018-06-21 13:39:00 +0200

    Updated style and added draggability

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <div class="title">Websites:</div>
-    <div class="website" v-for="website in websites"><img height="14px" :src="`https://logo.clearbit.com/${website.url.replace(/(^\w+:|^)\/\//, '').split('/')[0]}`"><a :href="website.url">{{website.label}}</a></div>
+    <draggable v-model="websites" @update="storeWebsiteState">
+      <div class="website" v-for="website in websites"><img height="14px" :src="`https://logo.clearbit.com/${website.url.replace(/(^\w+:|^)\/\//, '').split('/')[0]}`"><a :href="website.url">{{website.url}}</a></div>
+    </draggable>
     <form @submit.prevent="addWebsite" class="website">
       <input type="url" v-model="urlInput" required placeholder="url">
       <input v-model="labelInput"  type="text" required placeholder="label">
@@ -12,29 +14,34 @@
 
 <script>
 // @ is an alias to /src
+import draggable from "vuedraggable";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   data() {
-      const websites = localStorage.getItem('websites')
+    const websites = localStorage.getItem("websites");
     return {
-      websites: websites && JSON.parse(websites) || [],
+      websites: (websites && JSON.parse(websites)) || [],
       urlInput: "",
       labelInput: ""
     };
   },
   components: {
-    HelloWorld
+    HelloWorld,
+    draggable
   },
-    methods: {
-        addWebsite() {
-            this.websites.push({ url: this.urlInput, label: this.labelInput })
-            localStorage.setItem('websites',JSON.stringify(this.websites))
-            this.urlInput = ''
-            this.labelInput = ''
-        }
+  methods: {
+    addWebsite() {
+      this.websites.push({ url: this.urlInput, label: this.labelInput });
+      this.storeWebsiteState()
+      this.urlInput = "";
+      this.labelInput = "";
     },
+    storeWebsiteState(){
+        localStorage.setItem("websites", JSON.stringify(this.websites));
+    }
+  }
 };
 </script>
 
@@ -42,11 +49,13 @@ export default {
 .website {
   display: flex;
   align-items: center;
-  a{
+  a {
     margin-left: 0.5em;
+    color: white;
+    text-decoration: none;
   }
 }
-  input {
-    margin-right: 1em;
-  }
+input {
+  margin-right: 1em;
+}
 </style>
